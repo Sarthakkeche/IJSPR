93021b1c8d511b4b75aa7449d088342423cce7b8
sarthakkeche <kechesarthak412@gmail.com>
2025-08-30 00:37:12 +0530

    testing4

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "*", methods: ["GET", "POST"], credentials: true }));
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Static file serving
 
